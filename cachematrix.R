cb0bcd190e0d2451653841f82fd4639d6a26966f
Chris Hoffmeister <chris.hoffmeister@gmail.com>
2015-01-21 14:47:16 -0500

    Init Commit RProg W3PA2
    Initial commit of RProg Week 3 Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions create a matrix object that can 
+## cache its inverse using the solve function.
 
-## Write a short comment describing this function
+## Creates a special matrix object, which is really 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # Set value of matrix    
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    
+    # Get the value of the matrix
+    get <- function() x
+    
+    # Set the inverse of the matrix
+    setinverse <- function(solve) i <<- solve
+    
+    # Get the inverse of the inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function creates the inverse of the matrix created by the obove function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    # Check to see if inverse has been created
+    i <- x$getmean()
+    
+    # If so, it gets the inverse from the cache
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    
+    # If not, it calculates the inverse of the data ...
+    data <- x$get()
+    i <- solve(data, ...)
+    
+    # ... and sets the inverse in the cache via the setinverse function
+    x$setinverse(i)
+    i    ## Returns a matrix that is the inverse of 'x'
 }
